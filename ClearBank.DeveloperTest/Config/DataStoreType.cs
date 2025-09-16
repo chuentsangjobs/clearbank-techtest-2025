6d81453024d2955e7e4feb2287e5dad2645fa1d3
Chuen Tsang <chuen.tsang.jobs@gmail.com>
2025-09-16 19:48:52 +0100

    Clean up using statements

--- ClearBank.DeveloperTest/Config/DataStoreType.cs
+++ ClearBank.DeveloperTest/Config/DataStoreType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ClearBank.DeveloperTest.Config
+﻿namespace ClearBank.DeveloperTest.Config
 {
     public enum DataStoreType
     {
